de2433b799fd04c4d9fee4af9093e90279c7da4e
alex2209alex <alex.pavel2002@gmail.com>
2024-11-07 22:23:53 +0200

    Initial commit

---
+++ backend/licitatie-curieri/src/main/java/ro/fmi/unibuc/licitatie_curieri/LicitatieCurieriApplication.java
@@ -0,0 +1,13 @@
+package ro.fmi.unibuc.licitatie_curieri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicitatieCurieriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicitatieCurieriApplication.class, args);
+	}
+
+}
---
+++ backend/licitatie-curieri/src/test/java/ro/fmi/unibuc/licitatie_curieri/LicitatieCurieriApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fmi.unibuc.licitatie_curieri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicitatieCurieriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
