eb4ef9cab6f884ae63e29a589575bb335ff3d81a
Alex Istrate <istrate353@gmail.com>
2024-12-24 16:39:10 +0200

    RestaurantsManageScreen file fix

---
+++ frontend/licitatie_curieri/lib/restaurant/screens/MenuItemsCreateScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MenuItemsCreateScreen extends StatelessWidget {
+  const MenuItemsCreateScreen({Key? key, required restaurantId}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
