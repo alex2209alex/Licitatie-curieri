07178c0a5c5efe1c961556d9591aa2f53d5ae579
Alex Istrate <istrate353@gmail.com>
2024-12-29 20:45:15 +0200

    Changes

--- backend/licitatie-curieri/src/main/java/ro/fmi/unibuc/licitatie_curieri/common/utils/LogMessageUtils.java
+++ backend/licitatie-curieri/src/main/java/ro/fmi/unibuc/licitatie_curieri/common/utils/LogMessageUtils.java
@@ -1,7 +1,5 @@
 package ro.fmi.unibuc.licitatie_curieri.common.utils;
 
-import java.util.Locale;
-
 public class LogMessageUtils {
     private LogMessageUtils() {}
 
@@ -14,9 +12,9 @@ public class LogMessageUtils {
     public static final String CREATE_ADDRESS = "createAddress operation was invoked for User %s";
 
     public static final String GET_RESTAURANTS = "getRestaurants operation was invoked for Address %s";
-    public static final String CREATE_RESTAURANT = "createRestaurant operation was invoked with name %s, address %s, latitude %f and longitude %f";
-    public static final String DELETE_RESTAURANT = "Restaurant with id: %d was deleted";
-    public static final String UPDATE_RESTAURANT_BY_NAME = "Restaurant with id: %d was updated with name: %s";
+    public static final String CREATE_RESTAURANT = "createRestaurant operation was invoked for Restaurant %s";
+    public static final String UPDATE_RESTAURANT = "updateRestaurant operation was invoked for Restaurant %s";
+    public static final String REMOVE_RESTAURANT = "removeRestaurant operation was invoked for Restaurant %s";
 
     public static final String CREATE_MENU = "createMenu operation was invoked with idRestaurant %d, name %s, price: %f, ingredientsList %s, photo %s and discount %f";
     public static final String DELETE_MENU = "Menu with id: %d was deleted";
