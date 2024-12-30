a824c57737609f6c3f184149d95c3273e78c2101
Alexandru Pavel <alex.pavel2002@gmail.com>
2024-12-30 06:42:42 +0200

    Create order

--- backend/licitatie-curieri/src/main/java/ro/fmi/unibuc/licitatie_curieri/domain/order/mapper/OrderMapper.java
+++ backend/licitatie-curieri/src/main/java/ro/fmi/unibuc/licitatie_curieri/domain/order/mapper/OrderMapper.java
@@ -1,19 +1,66 @@
-//package ro.fmi.unibuc.licitatie_curieri.domain.order.mapper;
-//
-//import org.mapstruct.Mapper;
-//import org.mapstruct.Mapping;
-//import org.mapstruct.ReportingPolicy;
-//import org.openapitools.model.*;
-//import ro.fmi.unibuc.licitatie_curieri.domain.order.entity.Order;
-//
-//@Mapper(
-//        componentModel = "spring",
-//        unmappedTargetPolicy = ReportingPolicy.ERROR
-//)
-//public interface OrderMapper {
-//    @Mapping(target = "id", ignore = true)
-//    @Mapping(target = "orderDateTime", expression = "java(java.time.LocalDateTime.now())")
-//    Order toOrder(CreateOrderDto createOrderDto);
-//
-//    OrderDetailsDto toOrderDetailsDto(Order order);
-//}
+package ro.fmi.unibuc.licitatie_curieri.domain.order.mapper;
+
+import lombok.val;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+import ro.fmi.unibuc.licitatie_curieri.controller.order.models.OrderCreationDto;
+import ro.fmi.unibuc.licitatie_curieri.controller.order.models.OrderCreationItemResponseDto;
+import ro.fmi.unibuc.licitatie_curieri.controller.order.models.OrderCreationResponseDto;
+import ro.fmi.unibuc.licitatie_curieri.domain.address.entity.Address;
+import ro.fmi.unibuc.licitatie_curieri.domain.order.entity.Order;
+import ro.fmi.unibuc.licitatie_curieri.domain.order.entity.OrderStatus;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.List;
+
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.ERROR
+)
+public interface OrderMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "deliveryPrice", source = "orderCreationDto.deliveryPriceLimit")
+    @Mapping(target = "auctionDeadline", expression = "java(this.getAuctionDeadline())")
+    @Mapping(target = "orderStatus", expression = "java(this.getOrderStatusForCreation())")
+    @Mapping(target = "courier", ignore = true)
+    @Mapping(target = "orderMenuItemAssociations", ignore = true)
+    Order mapToOrder(OrderCreationDto orderCreationDto, double foodPrice, Address address);
+
+    @Mapping(target = "addressId", source = "address.id")
+    @Mapping(target = "auctionDeadline", source = "auctionDeadline", qualifiedByName = "toOffsetDateTime")
+    @Mapping(target = "deliveryPriceLimit", source = "deliveryPrice")
+    @Mapping(target = "price", source = "foodPrice")
+    @Mapping(target = "items", source = ".", qualifiedByName = "getOrderCreationItemResponseDtos")
+    OrderCreationResponseDto mapToOrderCreationResponse(Order order);
+
+    default Instant getAuctionDeadline() {
+        return Instant.now().plusSeconds(180);
+    }
+
+    default OrderStatus getOrderStatusForCreation() {
+        return OrderStatus.IN_AUCTION;
+    }
+
+    @Named("toOffsetDateTime")
+    default OffsetDateTime toOffsetDateTime(Instant instant) {
+        return OffsetDateTime.ofInstant(instant, ZoneId.systemDefault());
+    }
+
+    @Named("getOrderCreationItemResponseDtos")
+    default List<OrderCreationItemResponseDto> getOrderCreationItemResponseDtos(Order order) {
+        return order.getOrderMenuItemAssociations().stream()
+                .map(orderMenuItemAssociation -> {
+                    val orderCreationItemResponseDto = new OrderCreationItemResponseDto();
+                    orderCreationItemResponseDto.setId(orderMenuItemAssociation.getId().getMenuItemId());
+                    orderCreationItemResponseDto.setName(orderMenuItemAssociation.getMenuItem().getName());
+                    orderCreationItemResponseDto.setQuantity(orderMenuItemAssociation.getQuantity());
+                    orderCreationItemResponseDto.setPrice(orderMenuItemAssociation.getMenuItem().getPrice() * orderMenuItemAssociation.getQuantity());
+                    return orderCreationItemResponseDto;
+                })
+                .toList();
+    }
+}
